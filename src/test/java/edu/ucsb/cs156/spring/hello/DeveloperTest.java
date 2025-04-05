8312f024815cf32a02b7057b374146fe0bf70db5
Tiffany Gu <tiffanygu@ucsb.edu>
2025-04-04 21:16:41 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -22,14 +22,12 @@ public class DeveloperTest {
 
     @Test
     public void getName_returns_correct_name() {
-        // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-s25-teams>
         assertEquals("Tiffany G", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
-
+    @Test
     public void getGithubId_returns_correct_githubId() {
         assertEquals("tiffanyxgu", Developer.getGithubId());
     }
